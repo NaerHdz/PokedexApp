b85a4b57b1fc686d62070b01c35be4d3bb0869d9
NaerHdz <36934563+NaerHdz@users.noreply.github.com>
2025-02-19 18:51:53 -0600

    UPD: Design DetailScreen

--- src/screens/DetailsScreen/DetailsScreen.tsx
+++ src/screens/DetailsScreen/DetailsScreen.tsx
@@ -1,23 +1,55 @@
 import React from 'react';
-import {Image, StyleSheet, Text, View} from 'react-native';
+import {Image, SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import Animated, {BounceIn, BounceOut} from 'react-native-reanimated';
 import colors from '../../constants/colors';
 
 export default function DetailsScreen(): React.JSX.Element {
   return (
-    <View>
-      <View>
+    <SafeAreaView>
+      <View style={{backgroundColor: colors.grass, height: 230}}>
+        <Text style={styles.h1}>Bulbasaur</Text>
         <Animated.Image
           entering={BounceIn.duration(1000)}
           source={{
             uri: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
           }}
-          style={{width: 300, height: 250}}
+          style={{width: 400, height: 280, alignSelf: 'center'}}
         />
       </View>
-      <Text>Bulbausar</Text>
-    </View>
+      <View style={styles.containterType}>
+        <Text style={[styles.type, {backgroundColor: colors.grass}]}>
+          Grass
+        </Text>
+        <Text style={[styles.type, {backgroundColor: colors.poison}]}>
+          Poison
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  h1: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    alignSelf: 'flex-start',
+    marginLeft: 30,
+    marginVertical: 10,
+  },
+  containterType: {
+    marginTop: 100,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
+  type: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    width: 150,
+    padding: 10,
+    borderRadius: 20,
+    textAlign: 'center',
+    textShadowColor: 'rgba(255, 255, 255,1)',
+    textShadowOffset: {width: -1, height: 1},
+    textShadowRadius: 10,
+  },
+});
